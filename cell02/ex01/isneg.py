c4cfa9d642c2a661836e8d1ea1967646156b9240
67125013THANA <67125013@kmitl.ac.th>
2025-09-16 06:30:34 -0700

    Update isneg.py

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,16 +1,10 @@
-while True:
-    user_input = input("Enter a number (or 'q' to quit): ")
+user_input = input("Enter a number: ")
 
-    # ถ้าอยากให้หยุดโปรแกรม ให้พิมพ์ q
-    if user_input.lower() == "q":
-        print("Goodbye!")
-        break
+number = float(user_input)
 
-    number = float(user_input)
-
-    if number < 0:
-        print("This number is negative.")
-    elif number > 0:
-        print("This number is positive.")
-    else:
-        print("This number is both positive and negative.")
+if number < 0:
+    print("This number is negative.")
+elif number > 0:
+    print("This number is positive.")
+else:
+    print("This number is both positive and negative.")
